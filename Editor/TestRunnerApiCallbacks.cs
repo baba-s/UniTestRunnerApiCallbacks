b94cafbd9d52c7c4996c1de793e229928b72f24a
baba_s <shotababa1206@gmail.com>
2020-10-06 20:45:37 +0900

    first commit

---
+++ Editor/TestRunnerApiCallbacks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor.TestTools.TestRunner.Api;
+
+namespace Kogane
+{
+	public sealed class TestRunnerApiCallbacks : ICallbacks
+	{
+		public Action<ITestAdaptor>       OnRunStarted   { get; set; }
+		public Action<ITestResultAdaptor> OnRunFinished  { get; set; }
+		public Action<ITestAdaptor>       OnTestStarted  { get; set; }
+		public Action<ITestResultAdaptor> OnTestFinished { get; set; }
+
+		void ICallbacks.RunStarted( ITestAdaptor testsToRun )
+		{
+			OnRunStarted?.Invoke( testsToRun );
+		}
+
+		void ICallbacks.RunFinished( ITestResultAdaptor result )
+		{
+			OnRunFinished?.Invoke( result );
+		}
+
+		void ICallbacks.TestStarted( ITestAdaptor test )
+		{
+			OnTestStarted?.Invoke( test );
+		}
+
+		void ICallbacks.TestFinished( ITestResultAdaptor result )
+		{
+			OnTestFinished?.Invoke( result );
+		}
+	}
+}
